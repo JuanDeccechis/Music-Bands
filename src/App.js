955d2ec8fbc758ce60612ae3b2d795b58ff214c8
jcdeccechis <jcdeccechis@technisys.com>
2021-09-19 18:37:29 -0300

    first API get

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Router from './pages/Router';
 function App() {
 
   localStorage.setItem("isLogged", "false");
+  localStorage.setItem("APIURL", "https://my-json-server.typicode.com/improvein/dev-challenge")
   return (
     <div className="app">
       <Router />
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,44 @@
 import React, { Component } from "react";
+import Button from "../components/button/Button";
 
 class HomePage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+        bands: null
+    }
+  }
+
+  componentDidMount() {
+    this.getBands()
+  }
+
+  getBands() {
+      const { bands } = this.state;
+      fetch(`${localStorage.getItem('APIURL')}/bands`)
+        .then( res => {
+            return res.json();
+        })
+            .then( data => {
+                this.setState({ bands: data })
+            })
   }
 
   render() {
+    const { bands } = this.state;
     return <article className="container">
         <p className="example-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore sed provident cupiditate vitae commodi quae, earum autem quaerat! Praesentium quis distinctio incidunt fugit, facilis odio illo quam error culpa nemo!
         </p>
+        <div className="espaciado">
+        {bands && 
+            <ul>
+                {bands.map((band, index) => 
+                    <li key={index}> {band.name} - {band.genreCode}</li>
+                )}
+            </ul>
+        }
+        </div>
     </article>;
   }
 }
