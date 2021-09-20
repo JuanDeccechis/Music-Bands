e4854aa9cba56a15d1680fd5c8de1ed1517700b3
jcdeccechis <jcdeccechis@technisys.com>
2021-09-19 21:01:30 -0300

    spinner with loading logic

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,45 +1,88 @@
 import React, { Component } from "react";
-import Button from "../components/button/Button";
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 class HomePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        bands: null
+        bands: null,
+        albums: null, 
+        genre: null,
     }
   }
 
   componentDidMount() {
-    this.getBands()
+    this.getDataFromApi("bands");
+    this.getDataFromApi("albums");
+    this.getDataFromApi("genre");
+   // window.setTimeout(() => { this.setState({ loaded: true}) }, 5000);
+    
   }
 
-  getBands() {
-      const { bands } = this.state;
-      fetch(`${localStorage.getItem('APIURL')}/bands`)
+  getDataFromApi(path) {
+      fetch(`${localStorage.getItem('APIURL')}/${path}`)
         .then( res => {
             return res.json();
         })
             .then( data => {
-                this.setState({ bands: data })
+                this.setState({ [path]: data });
+                
             })
   }
 
   render() {
-    const { bands } = this.state;
+    const { bands, albums, genre } = this.state;
+    let loaded = bands && albums && genre;
+    if (!loaded) {
+        return <Loader
+                type="TailSpin"
+                color="#00BFFF"
+                height={100}
+                width={100}
+                className="loader"
+              />
+    } else {
+
     return <article className="container">
         <p className="example-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore sed provident cupiditate vitae commodi quae, earum autem quaerat! Praesentium quis distinctio incidunt fugit, facilis odio illo quam error culpa nemo!
         </p>
         <div className="espaciado">
-        {bands && 
-            <ul>
-                {bands.map((band, index) => 
-                    <li key={index}> {band.name} - {band.genreCode}</li>
-                )}
-            </ul>
-        }
+            <div className="results">
+                {bands && 
+                    <ul>
+                        {bands.map((band, index) => 
+                            <li key={index}> {band.id} , {band.name} , {band.genreCode}, {band.year}, {band.country}, </li>
+                        )}
+                    </ul>
+                }
+            </div>
+        </div>
+        <div className="espaciado">
+            <div className="results">
+                {albums && 
+                    <ul>
+                        {albums.map((album, index) => 
+                            <li key={index}> {album.id} , {album.bandId}, {album.name} , {album.year}</li>
+                        )}
+                    </ul>
+                }
+            </div>
+        </div>
+        <div className="espaciado">
+            <div className="results">
+                {genre && 
+                    <ul>
+                        {genre.map((gen, index) => 
+                            <li key={index}> {gen.code} , {gen.name}</li>
+                        )}
+                    </ul>
+                }
+            </div>
         </div>
     </article>;
+    }
   }
 }
 
